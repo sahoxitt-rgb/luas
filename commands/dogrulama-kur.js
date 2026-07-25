e2c3496f8ad45fac791dac12cb4c758e837bd9ea
sahoxitt-rgb <sahoxitt@gmail.com>
2026-07-25 19:46:43 +0300

    l

--- commands/dogrulama-kur.js
+++ commands/dogrulama-kur.js
@@ -20,7 +20,6 @@ module.exports = {
                 `> To gain full access to the server and view all channels, please verify by clicking the **🇬🇧 English (EN)** button below.\n\n` +
                 `❗️ \`Bilgi / Info\` __**SEÇTİĞİNİZ DİLE GÖRE KANALLAR AÇILACAKTIR / CHANNELS WILL BE UNLOCKED BASED ON YOUR LANGUAGE**__`
             )
-            .setImage('https://i.imgur.com/Line.png') // Araya o şık mor çizgiyi çeker
             .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
             .setFooter({ text: 'Luas • Doğrulama Sistemi / Verification System', iconURL: interaction.guild.iconURL() });
 
@@ -39,6 +38,6 @@ module.exports = {
         await interaction.channel.send({ embeds: [embed], components: [row] });
         
         // Sana onay mesajı atıyoruz
-        await interaction.editReply({ content: '✅ **Doğrulama paneli yepyeni ve şık tasarımıyla kanala atıldı!**' });
+        await interaction.editReply({ content: '✅ **Doğrulama paneli resimsiz ve sade tasarımıyla kanala atıldı!**' });
     }
 };
